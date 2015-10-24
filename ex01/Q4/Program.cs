c6903310b53ad58facf6e40eed5402519bd8be75
idoz <ido.ze2@gmail.com>
2015-10-24 13:20:43 +0300

    -

---
+++ ex01/Q4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Minutes:");
+            int min = int.Parse(Console.ReadLine());
+            int hour = min / 60;
+            Console.WriteLine("The Number In Hours Is {0} and {1} Minutes.", hour, (min - hour * 60));
+            Console.Read();
+        }
+    }
+}
