253763a23f9f5434960a588047bea691561782c7
idoz <ido.ze2@gmail.com>
2015-10-24 13:15:14 +0300

    -

---
+++ ex01/Q2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Tf;
+            double Tc;
+
+            Console.Write("Enter the temperature in Fahrenheit: ");
+            Tf = double.Parse(Console.ReadLine());
+            Tc = ((Tf - 32) * 5) / 9;
+
+            Console.WriteLine("The Temperature in Celcius Is " + Tc + " Degrees.");
+            Console.Read();
+        }
+    }
+}
