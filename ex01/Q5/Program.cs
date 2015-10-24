e6cb62f6f2e28ee4cd85a7401e2ea420218b53fe
idoz <ido.ze2@gmail.com>
2015-10-24 13:21:11 +0300

    -

---
+++ ex01/Q5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Amount Of Nails:");
+            int nails = int.Parse(Console.ReadLine());
+            int Boxes = nails / 50;
+            int Left = nails % 50;
+            Console.WriteLine("For {0} Nails, you need to order {1} Boxes, and there will be {2} Leftover.", nails, Boxes, Left);
+            Console.Read();
+        }
+    }
+}
