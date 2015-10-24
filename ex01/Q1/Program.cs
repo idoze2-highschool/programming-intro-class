1fa188445a85c2927bf8d87dd6c81b14d41a7b8c
idoz <ido.ze2@gmail.com>
2015-10-24 13:14:08 +0300

    -

---
+++ ex01/Q1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Side1;
+            int Side2;
+            int SurfaceArea;
+            int Perimeter;
+
+            Console.Write("Enter The First Side Of the Rectangle: ");
+            Side1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter The Second Side Of the Rectangle: ");
+            Side2 = int.Parse(Console.ReadLine());
+            SurfaceArea = Side2 * Side1;
+            Perimeter = (2 * Side1) + (Side2 * 2);
+
+            Console.WriteLine("The Surface Equals to " + SurfaceArea);
+            Console.WriteLine("The Perimeter Equals to " + Perimeter);
+
+            Console.Read();
+        }
+    }
+}
