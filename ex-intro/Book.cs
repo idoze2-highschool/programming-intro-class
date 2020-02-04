33a7a3e3880e8544d7ef8f9be554f0fe6e31893c
idoz <ido.ze2@gmail.com>
2020-02-04 19:25:24 +0200

    -

---
+++ ex-intro/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string title;
+            string auth;
+            double price;
+            int pages;
+
+            title = "Rebirth For Dummies";
+            auth = "Ed McMillan";
+            price = 14.5;
+            pages = 143;
+
+            Console.WriteLine("The book " + title + " by " + auth + " costs "+price+" Dollars, and it has "+pages+" Pages.");
+            Console.Read();
+        }
+    }
+}
