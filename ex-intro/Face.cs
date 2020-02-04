0aa314147930e15348ac80bc2278e896f3b742c2
idoz <ido.ze2@gmail.com>
2020-02-04 19:26:07 +0200

    -

---
+++ ex-intro/Face.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("#######   #######");
+            Console.WriteLine("#  @  #   #  @  #");
+            Console.WriteLine("# @@@ #   # @@@ #");
+            Console.WriteLine("#  @  #   #  @  #");
+            Console.WriteLine("#######   #######");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("        ^");
+            Console.WriteLine("       <*>");
+            Console.WriteLine("        !");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("+-+-+-+-+-+-+-+-+");
+            Console.Read();
+        }
+    }
+}
