6637d6e5b182847c1e9fc6309d4d8c2c29936f53
idoz <ido.ze2@gmail.com>
2020-02-05 05:43:15 +0200

    -

---
+++ ex10/Class.Color.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication10
+{
+    class Color
+    {
+        public static void Random()
+        {
+            Random random = new Random();
+            int c = random.Next(1, 5);
+            if (c == 1)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            if (c == 2)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+            }
+            if (c == 3)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            if (c == 4)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            }
+        }
+    }
+}
